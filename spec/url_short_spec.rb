e21c86f9a756c001b1a03d7aa27d66460e20f7c9
Emily Platzer <emilyplatzer@gmail.com>
2014-03-29 12:14:51 -0600

    User able to shorten a url
    Styling still needs work

--- spec/url_short_spec.rb
+++ spec/url_short_spec.rb
@@ -8,4 +8,18 @@ feature "User can shorten a URL" do
     visit '/'
     expect(page).to have_button("Shorten")
   end
+
+  scenario "User completes form and gets a shortened URL" do
+    visit '/'
+
+    fill_in "url_to_shorten", with: "www.google.com"
+    click_on "Shorten"
+
+    expect(page).to have_content("Original URL")
+    expect(page).to have_content("www.google.com")
+    expect(page).to have_content('"Shortened" URL')
+    expect(page).to have_content("tny.herokuapp.com/1")
+    expect(page).to have_content("'Shorten' another URL")
+
+  end
 end
--- url_short_app.rb
+++ url_short_app.rb
@@ -1,7 +1,15 @@
 require 'sinatra/base'
 
 class UrlShortApp < Sinatra::Application
+  URLS = []
+
   get '/' do
     erb :index
+  end
+
+  post '/:id' do
+    original_url = params[:url_to_shorten]
+    URLS << { original_url => "tny.herokuapp.com/#{params[:id]}" }
+    erb :show, locals: { :url_index => params[:id].to_i-1, :original_url => original_url }
   end
 end
