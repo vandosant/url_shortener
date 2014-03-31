bc4f61af7cd1b3e9cf65996fa9633c89e5716e84
Emily Platzer <emilyplatzer@gmail.com>
2014-03-31 09:03:06 -0600

    User can use the shortened URL

--- spec/url_short_spec.rb
+++ spec/url_short_spec.rb
@@ -4,22 +4,12 @@ require_relative ('../url_short_app')
 Capybara.app = UrlShortApp
 
 feature "User can shorten a URL" do
-  scenario "User can visit homepage and sees a form" do
-    visit '/'
-    expect(page).to have_button("Shorten")
-  end
-
-  scenario "User completes form and gets a shortened URL" do
+  scenario "User able to use the URL they input" do
     visit '/'
 
     fill_in "url_to_shorten", with: "www.google.com"
     click_on "Shorten"
 
-    expect(page).to have_content("Original URL")
-    expect(page).to have_content("www.google.com")
-    expect(page).to have_content('"Shortened" URL')
-    expect(page).to have_content("tny.herokuapp.com/1")
-    expect(page).to have_content("'Shorten' another URL")
-
+    click_link("tny.herokuapp.com/1")
   end
 end
