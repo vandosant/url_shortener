7d08e69a4c0738c430ccd95eee96f0f357191ffc
Emily Platzer <emilyplatzer@gmail.com>
2014-03-31 13:57:59 -0600

    [#68388204] User can shorten a URL
    WIP, not yet styled
    
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- url_short_app.rb
+++ url_short_app.rb
@@ -1,7 +1,20 @@
 require 'sinatra/base'
 
 class UrlShortApp < Sinatra::Application
+  URLS = []
+
   get '/' do
     erb :index
+  end
+
+  post '/' do
+    original_url = params[:url_to_shorten]
+    URLS << { original_url => "tny.herokuapp.com/#{URLS.length+1}" }
+    this_path = "/#{URLS.length.to_s}"
+    redirect this_path
+  end
+
+  get '/:id' do
+    erb :show, locals: { :url_index => params[:id].to_i-1, :original_url => URLS[(params[:id].to_i)-1].keys[0] }
   end
 end
